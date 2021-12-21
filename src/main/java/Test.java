8221c19dace6d974b28259f2b0a4af99994309e7
vrao <varunkadaparthi.rao@amadeus.com>
2021-12-21 14:33:43 +0530

    Updated Insertion sort to handle descending order as well

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,7 +2,7 @@ import sort.InsertionSort;
 
 public class Test {
     public static void main(String[] args) {
-        int[] arr = InsertionSort.sort(new int[]{5, 10, 25, 7, 23});
+        int[] arr = InsertionSort.sort(new int[]{5, 10, 25, 7, 23}, false);
         for (int num: arr) {
             System.out.println(num);
         }
--- src/main/java/sort/InsertionSort.java
+++ src/main/java/sort/InsertionSort.java
@@ -1,16 +1,38 @@
 package sort;
 
 public class InsertionSort {
-    public static int[] sort(int []arr) {
+
+    /**
+     * Ascending Sort
+     * @param arr - input array to be sorted
+     * @param ascending - order of sorting
+     * @return sorted array
+     */
+    public static int[] sort(int []arr, boolean ascending) {
         int i,j,key;
-        for(i = 1; i < arr.length; i++) {
-            key = arr[i];
-            j = i - 1;
-            while ( j >= 0 && arr[j] > key) {
-                arr[j+1] = arr[j];
-                j = j -1;
+        // sort in ascending order
+        if (ascending) {
+            for (i = 1; i < arr.length; i++) {
+                key = arr[i];
+                j = i - 1;
+                while (j >= 0 && arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
             }
-            arr[j+1] = key;
+        }
+        // sort in descending order
+        else {
+            for (i = arr.length-2; i >= 0; i--) {
+                key = arr[i];
+                j = i + 1;
+                while (j < arr.length && key < arr[j]) {
+                    arr[j-1] = arr[j];
+                    j++;
+                }
+                arr[j-1] = key;
+            }
         }
         return arr;
     }
