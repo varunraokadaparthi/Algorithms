75209ddf585bd831aeb6e0cb6a7813482aed7237
varunraokadaparthi <varunraok123@gmail.com>
2023-06-16 20:56:54 -0400

    Permutation in String Optimized

--- leetcode/src/main/java/com/algo/leetcode/slidingwindow/PermutationInString.java
+++ leetcode/src/main/java/com/algo/leetcode/slidingwindow/PermutationInString.java
@@ -17,6 +17,54 @@ public class PermutationInString {
     if (s1.length() > s2.length()) {
       return false;
     }
+    Map<Character, Integer> s1Map = new HashMap<>(26);
+    Map<Character, Integer> s2Map = new HashMap<>(26);
+    for (byte i = 0; i < 26; i++) {
+      s1Map.put((char) ('a' + i), 0);
+      s2Map.put((char) ('a' + i), 0);
+    }
+    for (char c : s1.toCharArray()) {
+      s1Map.put(c, s1Map.get(c) + 1);
+    }
+    int leftPointer = 0;
+    int rightPointer;
+    for (rightPointer = 0; rightPointer < s1.length(); ++rightPointer) {
+      s2Map.put(s2.charAt(rightPointer), s2Map.get(s2.charAt(rightPointer)) + 1);
+    }
+
+    byte matches = 0;
+    for (Map.Entry<Character, Integer> set : s1Map.entrySet()) {
+      if (set.getValue() == s2Map.get(set.getKey())) {
+        matches++;
+      }
+    }
+
+    while (rightPointer < s2.length()) {
+      if (matches == 26) {
+        return true;
+      }
+      s2Map.put(s2.charAt(leftPointer), s2Map.get(s2.charAt(leftPointer)) - 1);
+      if (s1Map.get(s2.charAt(leftPointer)) == s2Map.get(s2.charAt(leftPointer)) + 1) {
+        matches--;
+      } else if(s1Map.get(s2.charAt(leftPointer)) == s2Map.get(s2.charAt(leftPointer))) {
+        matches++;
+      }
+      leftPointer++;
+      s2Map.put(s2.charAt(rightPointer), s2Map.get(s2.charAt(rightPointer)) + 1);
+      if (s1Map.get(s2.charAt(rightPointer)) == s2Map.get(s2.charAt(rightPointer)) - 1) {
+        matches--;
+      } else if(s1Map.get(s2.charAt(rightPointer)) == s2Map.get(s2.charAt(rightPointer))) {
+        matches++;
+      }
+      rightPointer++;
+    }
+    return matches == 26;
+  }
+
+  public boolean checkInclusionUnOptimized(String s1, String s2) {
+    if (s1.length() > s2.length()) {
+      return false;
+    }
     Map<Character, Integer> s1Map = new HashMap<>();
     for (char c : s1.toCharArray()) {
       s1Map.put(c, s1Map.getOrDefault(c, 0) + 1);
